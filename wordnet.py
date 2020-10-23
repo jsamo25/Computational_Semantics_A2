c067811c61163259c85b3eb01950b8739d00cbc7
Jorge <jsamo25@gmail.com>
2020-10-23 23:12:56 +0200

    Part B. WordNet hypernyms extractions (first approach)

--- wordnet.py
+++ wordnet.py
@@ -1,16 +1,54 @@
 from nltk.corpus import wordnet as wn
+from itertools import chain
+from pdb import set_trace
 
 """
         PART A; WordNet synsets and lemmas
 """
-#3. how many wordnet synset does the word "study" has? how many are nouns?
-print("All synsets: ", wn.synsets("study"))
-print("the number of Nouns is:",len(wn.synsets("study",pos=wn.NOUN)))
+# 3 print number of synsets of a word, and number of NOUNS
+word_synsets = wn.synsets("study")
+word_synsets_noun = wn.synsets("study", pos=wn.NOUN)
 
-#4 for every noun synset of study print the synset's name, definition and examples.
-for syns in wn.synsets("study"):
-    print("Name: {}, Definition: {}, Examples: {}".format(syns.name(), syns.definition(), syns.examples()))
+print(
+    'The Number of synsets for "study" is {} from which {} are nouns'.format(
+        len(word_synsets), len(word_synsets_noun)
+    )
+)
+# alt: print(sum(['.n.' in study_synset.name() for study_synset in study_synsets]))
 
-#5 print all words the synset contains, i.e. the lemmas
-for syns in wn.synsets("study"):
-    print(syns.lemmas())
+# 4, #5 for every noun synset of study print the synset's name, definition, examles an lema names.
+for syn in word_synsets_noun:
+    print(
+        "\nName: {} \nDef: {} \nExamples: {} \nLemmas: {}".format(
+            syn.name(), syn.definition(), syn.examples(), syn.lemma_names()
+        )
+    )
+# print([syn.lemma_names() for syn in study_synsets])
+
+"""
+        PART B; WordNet taxonomic relations
+"""
+
+# 12 Hyponymy chain
+bass_syns = wn.synsets("bass")
+bass_syns_book = [syn for syn in bass_syns if ".03" in syn.name() or ".07" in syn.name()]
+
+for syn in bass_syns_book:
+    print(
+        "\nName: {} \nDef: {} \nExamples: {} \nLemmas: {}".format(
+            syn.name(), syn.definition(), syn.examples(), syn.lemma_names()
+        )
+    )
+
+print(bass_syns_book)
+print(bass_syns_book[0].lemma_names())
+print(bass_syns_book[1].lemma_names())
+
+def extract_hypernyms (synset):
+    return [word.lemma_names() for word in synset.hypernyms()][0]
+
+def request_hypernyms (syn_list):
+    for syn in syn_list:
+        print(extract_hypernyms(syn))
+
+request_hypernyms(bass_syns_book)
