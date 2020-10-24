29fde14cf20f04f1907d4cf37a6d24ff57c5c06f
Jorge <jsamo25@gmail.com>
2020-10-24 18:51:53 +0200

    Part C. sketch

--- wordnet.py
+++ wordnet.py
@@ -1,19 +1,20 @@
+import pandas as pd
+import numpy as np
 from nltk.corpus import wordnet as wn
 from pprint import pprint
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 from pdb import set_trace
 
 """
         PART A; WordNet synsets and lemmas
 """
 
-
 def get_synsets(word):
     return wn.synsets(word)
 
-
 def get_synsets_noun(word):
     return wn.synsets(word, pos=wn.NOUN)
-
 
 def synset_description(synset_list):
     for syn in synset_list:
@@ -22,7 +23,6 @@ def synset_description(synset_list):
                 syn.name(), syn.definition(), syn.examples(), syn.lemma_names()
             )
         )
-
 
 def part_a(word):
     print('Number of synsets of "{}" are {}'.format(word, len(get_synsets(word))))
@@ -34,10 +34,9 @@ def part_a(word):
         PART B; WordNet taxonomic relations
 """
 
-# 12 Hypernym chain
+
 def filter_index(synset_list, index_list):
     return [syn for syn in synset_list for index in index_list if index in syn.name()]
-
 
 def get_hypernyms_tree(synset_list):
     for syn in synset_list:
@@ -45,36 +44,54 @@ def get_hypernyms_tree(synset_list):
         hyp = lambda s: s.hypernyms()
         pprint(syn.tree(hyp))
 
-
 def get_taxonomic_distance(synset_1, synset_2):
     print()
     print(
         "path_similarity between {} and {} is: {}".format(
-            synset_1, synset_2, synset_1.path_similarity(synset_2)
-        )
-    )
+            synset_1, synset_2, synset_1.path_similarity(synset_2)))
     print(
         "lch_similarity between {} and {} is: {}".format(
-            synset_1, synset_2, synset_1.lch_similarity(synset_2)
-        )
-    )
+            synset_1, synset_2, synset_1.lch_similarity(synset_2)))
     print(
         "wup_similarity between {} and {} is: {}".format(
-            synset_1, synset_2, synset_1.wup_similarity(synset_2)
-        )
-    )
-
+            synset_1, synset_2, synset_1.wup_similarity(synset_2)))
 
 def part_b(word):
     print("\nThis synsets were filtered to match the figure 19.5 of Jurafsky")
     synset_list = filter_index(synset_list=get_synsets(word), index_list=[".03", ".07"])
     get_hypernyms_tree(synset_list=synset_list)
-    get_taxonomic_distance(synset_1=synset_list[0],synset_2=synset_list[1])
+    get_taxonomic_distance(synset_1=synset_list[0], synset_2=synset_list[1])
 
 
-if __name__ == "__main__":
-    # part_a(word="study")
-    part_b(word="bass")
+"""
+        PART C; Word sense disambiguation
+"""
+#FIXME: find the correct features and target
+pd.set_option("display.max_columns", 10)
+data = pd.read_csv("semcor30.csv")
+
+data_train, data_test = train_test_split(data, test_size=0.3)
+
+x_train = data_train[...]
+x_test = data_test[...]
+y_train, y_test = data_train[...], data_test[...]
+
+
+def accuracy(model, x_train, y_train, x_test, y_test):
+    print("training set:", model.score(x_train, y_train))
+    print("testing set:", model.score(x_test, y_test))
+
+
+print("\n Initial model score")
+model = LogisticRegression()
+#model.fit(x_train, y_train)
+# accuracy(model, x_train, y_train, x_test, y_test)
+# print("\n Initial model Coefficients", model.coef_.squeeze())
+
+
+# if __name__ == "__main__":
+# part_a(word="study")
+# part_b(word="bass")
 
 
 # TODO: part_b() print .lemma_names() not synset
