9da10307dc7c0082668107f144347ef726923d3b
Jorge <jsamo25@gmail.com>
2020-10-30 22:40:03 +0100

    Sentence Similarity: completed

--- embeddings.py
+++ embeddings.py
@@ -46,19 +46,18 @@ def dot(vector1,vector2):
 
 def cosine(vector1,vector2):
     return dot(vector1,vector2) / (norm(vector1)*norm(vector2))
-#print(cosine(word2vec["cat"],word2vec["dog"]))
+#print(cosine(word2vec["up"],word2vec["down"]))
 
 def word_cosine_similarity(word1,word2):
     print("The cosine similarity of {} and {} is:".format(word1,word2))
     return cosine(word2vec[word1],word2vec[word2])
 #print(word_cosine_similarity("apple","banana"))
-#vectors = np.array(vectors)
 
 def get_cosine_pairs(target):
     cosine_pairs = {}
     for word in words:
         if word == target:
-            continue #remove comparison with self
+            continue #remove comparison with self (always = 1.0)
         cosine_pairs[target,word] = cosine(word2vec[target],word2vec[word])
     return cosine_pairs
 
@@ -67,15 +66,18 @@ def get_n_similar_words(target,n):
     most_common = k.most_common(n)
     print("{} most (cosine) similar pairs:".format(n))
     pprint(most_common)
-#get_n_similar_words("dog", n = 10)
+#get_n_similar_words("student", n = 10)
 
-def get_sentence_embeddings(sentence):
+def sentence2vec(sentence):
     sentence = sentence.lower().split()
     sentence_embeddings = np.array([word2vec[word] for word in sentence])
     return np.average(sentence_embeddings, axis=0)
 
 def sentence_cosine_similarity(sentence1,sentence2):
     print("The cosine similarity of \"{}\" and \"{}\" is:".format(sentence1,sentence2))
-    return cosine(get_sentence_embeddings(sentence1),get_sentence_embeddings(sentence2))
+    return cosine(sentence2vec(sentence1),sentence2vec(sentence2))
 
-print(sentence_cosine_similarity("hello wonderful world","goodbye terrible world"))
+#print(sentence_cosine_similarity("hello world","goodbye world"))
+#print(sentence_cosine_similarity("dog","cat"))
+#print(sentence_cosine_similarity("flowers are red","violets are blue"))
+#print(sentence_cosine_similarity("i am very hungry", "blue rocket"))
