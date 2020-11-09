9a3f270414ed1f740b832f2986cb5ebe704d8487
Jorge <jsamo25@gmail.com>
2020-11-09 19:27:01 +0100

    Using a small sample of Semcor.csv as corpus, to train a gensim model and plot how close are words there.

---
+++ plot_gensim_model.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+import nltk
+import re
+
+from matplotlib import pyplot
+from gensim.models import Word2Vec
+from sklearn.decomposition import PCA
+from gensim.models import Word2Vec
+from pdb import set_trace
+
+pd.set_option("display.max_columns", 20)
+pd.set_option('display.width', 1000)
+pd.set_option("max_colwidth", 1000)
+
+data = pd.read_csv("semcor.csv")[100:200]
+
+def get_full_sentence(target_word, context_before, context_after):
+    return str(context_before) + " " + str(target_word) + " " + str(context_after)
+data["full_sentence"] = data[["target_word", "context_before", "context_after"]].apply(lambda x: get_full_sentence(*x), axis=1)
+
+#clean sentence
+STOP_WORDS = nltk.corpus.stopwords.words()
+def clean_sentence(context):
+    "remove chars that are not letters or numbers, downcase, then remove stop words"
+    regex = re.compile('([^\s\w]|_)+')
+    sentence = regex.sub('', context).lower()
+    sentence = sentence.split(" ")
+
+    for word in list(sentence):
+        if word in STOP_WORDS:
+            sentence.remove(word)
+
+    sentence = " ".join(sentence)
+    return sentence
+data["full_sentence"] = data["full_sentence"].apply(clean_sentence)
+
+#tokenize and define corpus: full context from semcor.csv
+corpus = [sentence.split() for sentence in data["full_sentence"]]
+
+#train the model
+model = Word2Vec(corpus, min_count=1)
+print(model)
+# print(list(model.wv.vocab))
+# print(model["Friday"])
+
+#reduce dimensionatlity to a 2d PCA model
+X = model[model.wv.vocab]
+pca = PCA(n_components=2)
+result = pca.fit_transform(X)
+
+#create the graph
+pyplot.scatter(result[:,0],result[:,1])
+words = list(model.wv.vocab)
+for i, word in enumerate(words):
+    pyplot.annotate(word, xy=(result[i,0], result[i,1]))
+
+pyplot.show()
